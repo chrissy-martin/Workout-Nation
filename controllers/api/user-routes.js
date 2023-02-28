a33ba58fe0f06bead2c72b3b9f106971421e885a
Andrew Chou <andrewchou1996@gmail.com>
2023-02-27 16:58:53 -0800

    delete unused code

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,10 +1,6 @@
 const router = require("express").Router();
 const { User } = require("../../models");
 
-// router.get("/", async (req, res) => {
-//   res.json({ message: "This is api/user" });
-// });
-
 router.post("/login", async (req, res) => {
   try {
     const userData = await User.findOne({ where: { email: req.body.email } });
